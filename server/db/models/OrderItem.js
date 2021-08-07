26669df9f5dd75f5dc1216af29d8c8b6e90724de
Ayan Haibeh <ayanyusufh@gmail.com>
2021-08-07 09:08:12 -0400

    add and fixed some models - going to merge during class

---
+++ server/db/models/OrderItem.js
@@ -0,0 +1,28 @@
+//usually ecommerce stores have a order line item that 
+//would have the products associated with that particular order
+
+//adding a model for that - we can edit it future wise if we go another path
+
+const Sequelize = require('sequelize');
+const { INTEGER, DECIMAL} = Sequelize;
+const db = require('../db');
+
+const OrderItem = db.define('orderItem', {
+   price: {
+       type: DECIMAL,
+       allowNull: false,
+       validate: {
+           notEmpty: true
+       }
+   },
+   quantity: {
+        type: INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        },
+        defaultValue: 1 //to make sure quantity doesn't fall to zero
+   }
+})
+
+module.exports = OrderItem;
