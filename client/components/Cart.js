403b4295d42014d684803575b72bd60209f6fb8f
SantaRedux <72110198+SantaRedux@users.noreply.github.com>
2021-08-13 18:20:13 -0500

    Update Cart.js

---
+++ client/components/Cart.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import ItemInCart from "./ItemInCart";
+
+const Cart = ({ cart }) => {
+
+
+
+    // useState(0) returns a tuple where the first parameter 
+    // count is the current state of the counter and setCounter 
+    // is the method that will allow us to update the counter's state.
+
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQty, setTotalQty] = useState(0);
+
+    useEffect(() => {
+        let items = 0;
+        let price = 0;
+
+        cart.cart.forEach((item) => {
+            items += item.qty;
+            price += item.qty * item.price;
+        });
+
+        setTotalQty(items);
+        setTotalPrice(price);
+    }, [cart, totalPrice, totalQty, setTotalPrice, setTotalQty]);
+
+
+
+    console.log('yo')
+    console.log('cart: ', cart)
+
+    return (
+        <div>
+            <div>
+                {cart.cart.map((item) => (
+                    <ItemInCart key={item.id} item={item} />
+                ))}
+            </div>
+            <div>
+                <h4>Cart Summary</h4>
+                <div>
+                    <span>Your total: {totalQty} items</span>
+                    <span>$ {totalPrice}</span>
+                </div>
+                <button>
+                    Proceed To Checkout
+                </button>
+            </div>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        cart: state.cart,
+    };
+};
+
+export default connect(mapStateToProps)(Cart);
