50cc51d923264f60ff0315033f1d2b41089ec438
SantaRedux <72110198+SantaRedux@users.noreply.github.com>
2021-08-14 09:59:20 -0500

    Update Cart.js

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import ItemInCart from "./ItemInCart";
+import { _fetchCart } from '../store/cart';
 
-const Cart = ({ cart }) => {
-    
+const Cart = (props) => {
+
     // useState(0) returns a tuple where the first parameter 
     // count is the current state of the counter and setCounter 
     // is the method that will allow us to update the counter's state.
 
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalQty, setTotalQty] = useState(0);
+    const { cart } = props; 
 
     useEffect(() => {
+
+        console.log('props: ', props)
+
+        if (props.auth.id) props.fetchCart(props.auth.id);
+
         let items = 0;
-        let price = 0;
+        let price = 0; 
 
         cart.cart.forEach((item) => {
             items += item.qty;
@@ -22,9 +29,13 @@ const Cart = ({ cart }) => {
 
         setTotalQty(items);
         setTotalPrice(price);
-    }, [cart.cart, totalPrice, totalQty, setTotalPrice, setTotalQty]);
-    
-    console.log('Cart.js cart: ', cart)
+    }, [props.auth]);
+    // [cart.cart, totalPrice, totalQty, setTotalPrice, setTotalQty]);
+
+
+
+    console.log('yo')
+    console.log('cart: ', cart)
 
     return (
         <div>
@@ -51,8 +62,13 @@ const Cart = ({ cart }) => {
 
 const mapStateToProps = (state) => {
     return {
+        auth: state.auth,
         cart: state.cart,
     };
 };
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = (dispatch) => ({
+    fetchCart: (userId) =>  dispatch(_fetchCart(userId))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
